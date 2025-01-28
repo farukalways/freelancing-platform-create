e61c0dd997089235b41f8558d2b5f30cf6a24329
farukalways <alishopon367@gmail.com>
2025-01-28 09:51:51 +0600

    basic setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [require('daisyui')],
-}
+  plugins: [daisyui],
+};
